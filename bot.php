9ae909be3808207f85d3ca57d38cf5ada396cf96
wimkung <nattapong_h@cmu.ac.th>
2017-05-01 13:55:53 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = '1PJqZhAv5RWER6iN7UmlY7pAP7Rv3HZtRXaMFgJewfnsx+qCXCIpV/YAI9mlcJ97tvu3cMvy4z/9cQZa0C8RCgey/yDYQP1VDYh2a7m/yCairQwOWnECaXm+zVvtFr3DhXX75BI5hIlZ6DY5QTE/PwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
